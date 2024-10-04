94e0a5971faf4e3a13a1d6320a4a7e283fa4f5bf
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-10-04 16:55:54 -0300

    Update consultora.py

--- consultora.py
+++ consultora.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import streamlit_authenticator as stauth
 from pathlib import Path
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
